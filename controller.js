16ca801ef8a2dd833a76339872bc3780cae3271e
Andietz <andietz.orion@gmail.com>
2022-12-24 15:47:55 +0700

    berhasil menambahkan data

--- controller.js
+++ controller.js
@@ -31,3 +31,27 @@ exports.tampilberdasarkanid = function(req,res){
         
         });
 };
+
+// menambahkan data game
+exports.tambahGame = function(req,res){
+    var id = req.body.id;
+    var provider_name = req.body.provider_name;
+    var game_name = req.body.game_name;
+    var img_url = req.body.img_url;
+    var rtp_percentage = req.body.rtp_percentage;
+    var bet_start = req.body.bet_start;
+    var bet_end = req.body.bet_end;
+    var gacor_time_start = req.body.gacor_time_start;
+    var gacor_time_end = req.body.gacor_time_end;
+
+    connection.query('INSERT INTO games (id,provider_name,game_name,img_url,rtp_percentage,bet_start,bet_end,gacor_time_start,gacor_time_end) VALUES (?,?,?,?,?,?,?,?,?)',
+        [id,provider_name,game_name,img_url,rtp_percentage,bet_start,bet_end,gacor_time_start,gacor_time_end],
+        function (error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok("Berhasil Menambah game Baru", res)
+            }
+        });
+
+};
